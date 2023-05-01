b004e97323fe7409e4ba1a11da3c319a1f998716
Beam <beam7684@gmail.com>
2023-05-01 23:41:57 +0700

    Install testing tool

---
+++ __test__/useCalendar.test.ts
@@ -0,0 +1,5 @@
+describe('useCalendar', () => {
+  it('should be defined', () => {
+    expect(1).toBe(1)
+  })
+})
---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  moduleNameMapper: {
+    '\\.(scss|sass|css)$': 'identity-obj-proxy',
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+    '^@/pages/(.*)$': '<rootDir>/pages/$1',
+    '^@/lib/(.*)$': '<rootDir>/lib/$1',
+  },
+}
